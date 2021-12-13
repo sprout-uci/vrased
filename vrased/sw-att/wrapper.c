cc10d5898cfb3f6c6d7799cc8524cf3179412d66
marton bognar <marton.bognar@cs.kuleuven.be>
2021-12-13 17:28:33 +0100

    Some refactoring

--- vrased/sw-att/wrapper.c
+++ vrased/sw-att/wrapper.c
@@ -1,5 +1,10 @@
 #include <string.h>
 
+/**
+ * Attack authors: our comments are added as block comments, the original
+ * VRASED comments are preserved as line comments.
+ * */
+
 /*********** TRUSTED VRASED WRAPPER CODE (inside SW-Att) ***********/
 
 #define MAC_ADDR 0x0230
@@ -7,7 +12,7 @@
 #define ATTEST_DATA_ADDR 0xE000
 #define ATTEST_SIZE 0x20
 
-// fields for VRASED_A, in unprotected DMEM for our PoC
+/* Fields for VRASED_A, in unprotected DMEM for our PoC */
 #define CTR_ADDR 0x0270
 #define VRF_AUTH 0x0250
 
@@ -24,10 +29,12 @@ hmac(
 
 int attack_iteration;
 
-// VRASED_A: authenticated attestation
-// we based the code on the RATA implementation, which only differs from the code
-// in the appendix of VRASED in one place: it uses the MAC region to pass the challenge
-// (which the base VRASED also does)
+/**
+ * VRASED_A: authenticated attestation
+ * We based the code on the RATA implementation, which only differs from the code
+ * in the appendix of VRASED in one place: it uses the MAC region to pass the challenge
+ * (which the base VRASED also does).
+ * */
 
 __attribute__ ((section (".do_mac.call"))) void Hacl_HMAC_SHA2_256_hmac_entry()
 {
@@ -68,15 +75,21 @@ __attribute__ ((section (".do_mac.call"))) void Hacl_HMAC_SHA2_256_hmac_entry() 
     //Save application stack pointer.
     //Allocate key buffer.
     #if __ATTACK == 3
-    // NOTE: we explicitly remove the (redundant) zero-initialization here, as
-    // it may generate an early reset when writing out of the MAC region.
-    // However, this is only needed when the compiler does not already optimize
-    // away the redundant zero-intialization when noticing the explicit memcpy
-    // initialization below (e.g., as validated with clang-msp430).
+
+    /**
+     * NOTE: we explicitly remove the (redundant) zero-initialization here, as
+     * it may generate an early reset when writing out of the MAC region.
+     * However, this is only needed when the compiler does not already optimize
+     * away the redundant zero-intialization when noticing the explicit memcpy
+     * initialization below (e.g., as validated with clang-msp430).
+     * */
+
     uint8_t key[64];
+
     #else
     uint8_t key[64] = {0};
     #endif
+
     //Copy the key from KEY_ADDR to the key buffer.
     memcpy(key, (uint8_t*)KEY_ADDR, 64);
     hmac((uint8_t*) key, (uint8_t*) key, (uint32_t) 64, (uint8_t*) MAC_ADDR, (uint32_t) 32);
@@ -114,11 +127,14 @@ __attribute__ ((section (".do_mac.leave"))) __attribute__((naked)) void Hacl_HMA
 
 #define KEY_SIZE                        64
 
-// MAC region: [0x0230, 0x250[
-// NOTE: leave 12 bytes for local variables of memcpy function
+/**
+ * MAC region: [0x0230, 0x250[
+ * NOTE: leave 12 bytes for local variables of memcpy function
+ * */
+
 #define STACK_POISON_ADDRESS            (MAC_ADDR + KEY_SIZE + 12)
 
-// stringify macro parameter a
+/* stringify macro parameter a */
 #define __s__(a)                        #a
 #define _s_(a)                          __s__(a)
 
@@ -144,15 +160,18 @@ void leak_key(uint8_t *buf, int start, int end)
 void VRASED (uint8_t *challenge, uint8_t *response) {
     printf("Attack: %d\n", __ATTACK);
 
-    #if __ATTACK == 4
-      attack4:
-      // modifying the counter address for demonstration purposes; normally this
-      // would also be protected (but its value is known to the attacker)
+    #if __ATTACK == 4 || __ATTACK == 5 || __ATTACK == 6
+    attack4:
+      /* modifying the counter address for demonstration purposes; normally this
+         would also be protected (but its value is known to the attacker) */
       my_memset(CTR_ADDR, 32, 0);
 
       my_memset(VRF_AUTH, 32, 0);
       uint8_t * verification = (uint8_t*) VRF_AUTH;
-      // correct verification value: 444eb44a4a018344b057451667ac6e8414f7736c329edd7fff8d467cb1f5c5d3
+      /* correct verification value: 444eb44a4a018344b057451667ac6e8414f7736c329edd7fff8d467cb1f5c5d3 */
+    #endif
+
+    #if __ATTACK == 4
       switch (attack_iteration) {
       case 0:
         verification[0] = 0x42;
@@ -168,28 +187,24 @@ void VRASED (uint8_t *challenge, uint8_t *response) {
         break;
       }
 
-      // setting up the clock for time measurement
+      /* setting up the clock for time measurement */
       TACTL = TACLR | MC_2 | TASSEL_2;
     #endif
 
     #if __ATTACK == 5
-      // modifying the counter address for demonstration purposes; normally this
-      // would also be protected (but its value is known to the attacker)
-      my_memset(CTR_ADDR, 32, 0);
-
-      my_memset(VRF_AUTH, 32, 0);
-
       if (*((uint16_t*) DMA_ATTACKER_PERSISTENT_FLAG)) {
         uint16_t delay = *((uint16_t*) DMA_ATTACKER_RESET_CNT);
         printf("Interrupt delay: %u\n", delay);
 
-        // in case the delay is 14486, it means the interrupt was served in one
-        // cycle: it hit the `mov.b` instruction (pc: b0b6) that is already
-        // outside of the comparison loop in `memcmp`.
-        // in case the delay is one cycle longer, that indicates interrupting
-        // during the `jmp` instruction (pc: b0be) that jumps back to the start
-        // of the comparison loop in `memcmp`, indicating that the first byte
-        // was correctly guessed
+        /**
+         * In case the delay is 14486, it means the interrupt was served in one
+         * cycle: it hit the `mov.b` instruction (pc: b0b6) that is already
+         * outside of the comparison loop in `memcmp`.
+         * In case the delay is one cycle longer, that indicates interrupting
+         * during the `jmp` instruction (pc: b0be) that jumps back to the start
+         * of the comparison loop in `memcmp`, indicating that the first byte
+         * was correctly guessed.
+         * */
 
         if (delay == 14486) {
           attack_iteration++;
@@ -202,8 +217,6 @@ void VRASED (uint8_t *challenge, uint8_t *response) {
         printf("First run\n");
       }
 
-      uint8_t * verification = (uint8_t*) VRF_AUTH;
-      // correct verification value: 444eb44a4a018344b057451667ac6e8414f7736c329edd7fff8d467cb1f5c5d3
       switch (attack_iteration) {
       case 0:
         verification[0] = 0x42;
@@ -215,29 +228,25 @@ void VRASED (uint8_t *challenge, uint8_t *response) {
         break;
       }
 
-      // setting up the clock for interrupting
+      /* setting up the clock for interrupting */
       TACCR0 = 26385;
       *((uint16_t*) DMA_ATTACKER_RESET_CNT) = 0;
       TACTL = TACLR | MC_1 | TASSEL_2 | ID_3 | TAIE;
     #endif
 
     #if __ATTACK == 6
-      // modifying the counter address for demonstration purposes; normally this
-      // would also be protected (but its value is known to the attacker)
-      my_memset(CTR_ADDR, 32, 0);
-
-      my_memset(VRF_AUTH, 32, 0);
-
       if (*((uint16_t*) DMA_ATTACKER_PERSISTENT_FLAG)) {
         uint16_t delayed = *((uint16_t*) DMA_ATTACKER_DELAYED);
         printf("DMA delayed: %s\n", delayed ? "Yes" : "No");
 
-        // similar to above, we aim the DMA data memory request to the same
-        // cycle when the `memcmp` loads the byte to be compared from data
-        // memory.
-        // if the contention happens, that means the next byte was fetched,
-        // indicating that the comparison for the current byte succeeded, the
-        // guessed value was correct.
+        /**
+         * Similar to above, we aim the DMA data memory request to the same
+         * cycle when the `memcmp` loads the byte to be compared from data
+         * memory.
+         * If the contention happens, that means the next byte was fetched,
+         * indicating that the comparison for the current byte succeeded, the
+         * guessed value was correct.
+         * */
 
         if (!delayed) {
           attack_iteration++;
@@ -250,8 +259,6 @@ void VRASED (uint8_t *challenge, uint8_t *response) {
         printf("First run\n");
       }
 
-      uint8_t * verification = (uint8_t*) VRF_AUTH;
-      // correct verification value: 444eb44a4a018344b057451667ac6e8414f7736c329edd7fff8d467cb1f5c5d3
       switch (attack_iteration) {
       case 0:
         verification[0] = 0x42;
@@ -263,9 +270,9 @@ void VRASED (uint8_t *challenge, uint8_t *response) {
         break;
       }
 
-      // setting up the dma
+      /* setting up the timed dma request */
       *((uint16_t*) DMA_ATTACKER_COUNTDOWN) = 26387;
-      __asm__ volatile("nop\n"); // skipping one cycle for precise timing
+      __asm__ volatile("nop\n"); /* skipping one cycle for precise timing */
     #endif
 
     #if __ATTACK == 1
@@ -277,15 +284,8 @@ void VRASED (uint8_t *challenge, uint8_t *response) {
       __asm__ volatile(
           "mov #1, &" _s_(DMA_ATTACKER_STEAL_KEY) "\n"
           ".REPT 64 \n"
-          "nop \n"
+          "  nop \n"
           ".ENDR \n"
-          "nop \n"
-          "nop \n"
-          "nop \n"
-          "nop \n"
-          "nop \n"
-          "nop \n"
-          "nop \n"
       );
       leak_key((uint8_t*) MAC_ADDR, 0, 64);
       return;
@@ -304,7 +304,7 @@ void VRASED (uint8_t *challenge, uint8_t *response) {
     my_memcpy ( (uint8_t*)MAC_ADDR, challenge, 32);
 
     #if __ATTACK == 5
-    // enable interrupts
+    /* enable interrupts */
     __eint();
     #else
     //Disable interrupts:
@@ -326,8 +326,8 @@ void VRASED (uint8_t *challenge, uint8_t *response) {
     __asm__ volatile("mov    r1,    r5" "\n\t");
 
     // Set the stack pointer to the base of the exclusive stack:
-    // NOTE: call will do a 2-byte push, hence desired address+2
     #if __ATTACK == 3
+      /* NOTE: call will do a 2-byte push, hence desired address + 2 */
       __asm__ volatile("mov #" _s_(STACK_POISON_ADDRESS + 2) ",     r1" "\n\t");
     #else
       __asm__ volatile("mov    #0x1002,     r1" "\n\t");
@@ -351,12 +351,11 @@ void VRASED (uint8_t *challenge, uint8_t *response) {
       printf("Attack iteration %d, execution took %d cycles (%d bytes correct)\n",
         attack_iteration,
         tar,
-        (tar - 14511) / 13);
+        (tar - 14511) / 13); /* Cycle count: 14511 + (guessed_byte_count * 13) */
       if (attack_iteration < 2) {
         attack_iteration++;
         goto attack4;
       }
-      // guessed cycles: 14511 + (guessed * 13)
     #endif
 
     // Return the HMAC value to the application:
